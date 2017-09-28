c8257ad0bab7dce4ee71bcad29b768dc905118c4
Hoofdgebruiker <ellen.cardoen@student.howest.be>
2017-09-28 11:33:02 +0200

    bla

---
+++ BasisOefeningen.py
@@ -0,0 +1,41 @@
+print("Hello world")
+print("Nog eens hallo ")
+# dit is commentaar
+#python interpreter gaat dit niet verwerken
+
+#python GEEN declaratie van types
+# int x = 3;
+#python ontdekt welk type het is
+#Hoe?
+x=3 #int, integer of geheel getal dakn ook negatief -3
+y = 3.0 #float door het punt
+z = "3" # met een string
+
+#commando type
+print("x is van het type" + str(type(x)))
+print(type(y))
+print(type(z))
+
+#naam = input("Tik aub je naam in")
+#print(naam)
+
+print("Een tekst met \"quotes\" rond?")
+
+print("Cardoen")
+print("Ellen")
+print("ellen.cardoen@student.howest.be")
+
+#Naam  = input("Geef je naam op")
+#Voornaam  = input("Geef je voornaam op")
+#Leeftijd = input("Geef je leeftijd op")
+
+#slechte code
+#print("Voornaam is: " + voornaam + "naam: " + naam)
+
+#veel beter is format gebruiken
+#getallen formateren en vraagt minder resources
+#print("Voornaam is: {0}  naam is: {1} leeftijd is {2}".format(Voornaam,Naam,Leeftijd) )
+
+
+
+
---
+++ demoGit.py
@@ -0,0 +1,3 @@
+print("Ellen")
+
+print("Cardoen")
